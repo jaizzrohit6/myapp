f95ec1fb7e3d1eeb6b4f0e01124e26e8839ce2a9
jaizzrohit6 <rohit1952425@gmail.com>
2021-06-13 11:18:30 +0530

    react project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  //BEM
+  return (
+    <div className="App">
+        <h1>Hello GeekyProgrammer lets build something exiceted project </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAsudeKQJbgf-p7jQ6OUMOkXsIhswCejZs",
+    authDomain: "mynewapp-6f228.firebaseapp.com",
+    projectId: "mynewapp-6f228",
+    storageBucket: "mynewapp-6f228.appspot.com",
+    messagingSenderId: "429125554716",
+    appId: "1:429125554716:web:a96e15d0ce6846556c45d9",
+    measurementId: "G-0T88M0MLL9"
+  };
